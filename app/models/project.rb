6198cf3ee666e196bd4621358d888ad2b9a9ffd9
Taylor <zeitue@gmail.com>
2022-02-11 23:07:31 -0600

    Cleaned up selects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
     validates :title, presence: true, allow_blank: false
     validates :identifier, presence: true, allow_blank: false
+
+    def to_s
+        self.identifier.to_s + '-' + self.title
+    end
 end
