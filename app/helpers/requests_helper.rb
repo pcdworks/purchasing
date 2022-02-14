a587eae4112cc705a47f6232c25d0b10fced5f05
Taylor <zeitue@gmail.com>
2022-02-13 22:53:24 -0600

    Added index and status

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,23 @@
 module RequestsHelper
+
+
+    def statuses
+        {
+            0 => 'Awaiting Approval',
+            1 => 'Approved',
+            2 => 'On Hold',
+            3 => 'Ordered',
+            4 => 'Received - Completed',
+            5 => 'Received - Partial',
+            6 => 'Received - Returned'
+        }
+    end
+
+    def shipping_charges
+        {
+            0 => 'Vendor',
+            1 => 'UPS Account',
+            2 => 'FedEx Account'
+        }
+    end
 end
