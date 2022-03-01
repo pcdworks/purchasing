6f996120fcf404e6c4f36aec2de9ee56973dc2c2
Taylor Ramirez <tramirez@pcdworks.com>
2022-03-01 15:55:38 -0600

    Added from to mailer

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,17 +1,19 @@
 class RequestMailer < ApplicationMailer
     def new_request_email
         @request = params[:request]
-    
+
         case @request.status
         when 1
             Account.where(approver: true).each do |account|
                 mail(
+                    from: ENV['MAILER_USER_NAME'],
                     to: account.email,
                     subject: "A new purchase request is awaiting approval: " + @request.to_s
                 )
             end
         when 2
             mail(
+                from: ENV['MAILER_USER_NAME'],
                 to: @request.account.email,
                 cc: @request.requested_for.email,
                 subject: "Purchase request has been approved: " + @request.to_s
