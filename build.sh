b9c9a7a224ce5f1063718ce9729e087a8244be1e
Taylor Ramirez <tramirez@pcdworks.com>
2026-04-28 14:19:50 -0500

    build bug fixed

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build . -f docker/Dockerfile -t pcdworks/purchasing:latest -t "pcdworks/purchasing:`git describe`"
+VERSION=$(git describe --tags --abbrev=0)
+docker build . -f docker/Dockerfile -t pcdworks/purchasing:latest -t "pcdworks/purchasing:${VERSION}"
